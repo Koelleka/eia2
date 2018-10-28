5eccbe61bc2140c5cdfdabaf9133ff172166bb09
katharinakolle <katharinakolle@katharinas-mbp.fritz.box>
2018-10-28 18:24:27 +0100

    Variablen umbenannt für die code convention

--- Aufgabe2/uno.js
+++ Aufgabe2/uno.js
@@ -1,109 +1,111 @@
 var uno;
 (function (uno) {
     // Mit den Infos bauen wir später alle Karten die es gibt
-    let farben = ["red", "blue", "green", "yellow"];
-    let aktionsKarten = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
-    let jokerKarten = ["FW", "+4"]; // 4 pro Joker
-    let zahlenKarten = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    //alle Farben die es gibt 
+    let cardColors = ["red", "blue", "green", "yellow"];
+    //Alle vorhandenen Aktionskarten ges. Spiel
+    let actionCards = ["+2", "<>", "A"]; // 12 stk / 2 pro Farbe / A = Aussetzen
+    let jokerCards = ["FW", "+4"]; // 4 pro Joker FW = Farbe wählen
+    let numberCards = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     // Alle noch verfügbaren Karten im Spiel
-    var kartendeck;
+    var deck;
     // Karten auf meine Hand
-    var meineKarten;
-    // Eine Hilfsklasse um eine Karte zu beschreiben (farbe + art)
-    class Karte {
+    var myCards;
+    // Eine Klasse um eine Karte zu beschreiben (farbe + art)
+    class Card {
         constructor(_farbe, _art) {
-            this.farbe = _farbe;
-            this.art = _art;
+            this.color = _farbe; //this=zugriff auf "farbe, art"
+            this.value = _art; //this=Eigenreferenz 
         }
     }
     // Spiel initialisieren
     function start() {
-        var str = prompt("Bitte die Spielkartenanzahl auswählen: ", "5");
-        var anzahl = Number(str);
-        erzeugeAlleKarten();
-        meineKarten = [];
+        //promt=Eingabefenster
+        var input = prompt("Bitte die Spielkartenanzahl auswählen: ", "5");
+        var anzahl = Number(input);
+        createAllCards(); //Funktionsaufruf
+        myCards = [];
         for (var i = 0; i < anzahl; i++) {
-            zufallskarteZiehen();
+            pickRandomCard(); //Funktionsaufruf
         }
-        meineKartenZeichnen();
+        drawMyCards(); //Funktionsaufruf
     }
-    function erzeugeAlleKarten() {
-        kartendeck = [];
-        for (var i = 0; i < farben.length; i++) {
-            var farbe = farben[i];
+    function createAllCards() {
+        deck = [];
+        for (var i = 0; i < cardColors.length; i++) {
+            var color = cardColors[i];
             // Aktionskarten
             for (var j = 0; j < 2; j++) {
-                for (var k = 0; k < aktionsKarten.length; k++) {
-                    var art = aktionsKarten[k];
-                    var karte = new Karte(farbe, art);
-                    kartendeck.push(karte);
+                for (var k = 0; k < actionCards.length; k++) {
+                    var art = actionCards[k];
+                    var karte = new Card(color, art); //constructor 
+                    deck.push(karte); //karte wird auf den stapel gelegt 
                 }
             }
             // Zahlenkarten
             for (j = 0; j < 4; j++) {
-                for (k = 0; k < zahlenKarten.length; k++) {
-                    art = zahlenKarten[k];
-                    karte = new Karte(farbe, art);
-                    kartendeck.push(karte);
+                for (k = 0; k < numberCards.length; k++) {
+                    art = numberCards[k];
+                    karte = new Card(color, art);
+                    deck.push(karte);
                 }
             }
         }
         // Jokerkarten
         for (i = 0; i < 4; i++) {
-            for (j = 0; j < jokerKarten.length; j++) {
-                art = jokerKarten[j];
-                karte = new Karte(farbe, art);
-                kartendeck.push(karte);
+            for (j = 0; j < jokerCards.length; j++) {
+                art = jokerCards[j];
+                karte = new Card(null, art);
+                deck.push(karte);
             }
         }
     }
-    function zufallskarteZiehen() {
-        console.log("Hello");
-        // Anzahl verbleibender Karten im Kartendeck
-        var anzahlVerbleibenderKartenImStapel = kartendeck.length;
-        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
-        var index = Math.floor(Math.random() * anzahlVerbleibenderKartenImStapel);
-        // Karte aus dem Kartendeck holen
-        var karte = kartendeck[index];
+    function pickRandomCard() {
+        // Anzahl verbleibender Karten im deck
+        let remainingCardsInDeck = deck.length;
+        // Zufälligen Index erzeugen, welche Karte wir aus dem deck nehmen
+        var index = Math.floor(Math.random() * remainingCardsInDeck); //floor schneidet komma zahl weg//
+        // Karte aus dem deck holen
+        var card = deck[index];
         // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
-        kartendeck.splice(index, 1);
-        meineKarten.push(karte);
+        deck.splice(index, 1);
+        myCards.push(card); //karte wird auf "meinen Stapel gelegt
     }
-    function meineKartenZeichnen() {
-        for (var i = 0; i < meineKarten.length; i++) {
-            var karte = meineKarten[i];
-            //<div class="card"> 
-            let card = document.createElement("div");
+    function drawMyCards() {
+        for (var i = 0; i < myCards.length; i++) {
+            var tmpCard = myCards[i];
+            //<div class="card">  //erzeugen vonHTMLtags, karten anzeigen
+            let card = document.createElement("div"); //document, html element erstellen typ div
             card.className = "card";
             // <div class="bg green"></div> 
             let bg = document.createElement("div");
-            bg.className = "bg " + karte.farbe;
-            card.appendChild(bg);
-            // <div class="circle"></div> 
+            bg.className = "bg " + tmpCard.color;
+            card.appendChild(bg); //fügt den hintergrund dieser Karte hinzu - verschachtelt angefügt
+            // <div class="circle"></div> //Ellipse der Karte hinzufügen
             let kreis = document.createElement("div");
             kreis.className = "circle";
-            card.appendChild(kreis);
+            card.appendChild(kreis); //fügt die Ellipse dieser einen Karte hinzu 
             //  <div class="top-left">9</div> 
             let obenLinks = document.createElement("div");
             obenLinks.className = "top-left";
-            obenLinks.innerHTML = karte.art;
-            card.appendChild(obenLinks);
+            obenLinks.innerHTML = tmpCard.value; //was kommt auf diese karte drauf idF Zahl
+            card.appendChild(obenLinks); //fügt die zahl oben links dieser Karte hinzu
             //  <div class="center">9</div> 
             let zentrum = document.createElement("div");
             zentrum.className = "center";
-            zentrum.innerHTML = karte.art;
-            card.appendChild(zentrum);
+            zentrum.innerHTML = tmpCard.value; //was kommt auf diese Karte idF die Zahl zentriert
+            card.appendChild(zentrum); //fügt die Zahl zentriert dieser Karte hinzu
             // <div class="bottom-right">9</div> 
             let untenRechts = document.createElement("div");
             untenRechts.className = "bottom-right";
-            untenRechts.innerHTML = karte.art;
+            untenRechts.innerHTML = tmpCard.value;
             card.appendChild(untenRechts);
             let style = card.style;
             style.left = 10 + i * 80 + "px";
             style.bottom = "10px";
-            document.body.appendChild(card);
+            document.body.appendChild(card); //Karte HTML body hinzufügen
         }
     }
-    document.addEventListener("DOMContentLoaded", start);
+    document.addEventListener("DOMContentLoaded", start); //
 })(uno || (uno = {}));
 //# sourceMappingURL=uno.js.map
--- Aufgabe2/uno.ts
+++ Aufgabe2/uno.ts
@@ -1,138 +1,142 @@
 namespace uno {
 
     // Mit den Infos bauen wir später alle Karten die es gibt
-    let farben: string[] = ["red", "blue", "green", "yellow"];
-    let aktionsKarten: string[] = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
-    let jokerKarten: string[] = ["FW", "+4"]; // 4 pro Joker
-    let zahlenKarten: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    //alle Farben die es gibt 
+    let cardColors: string[] = ["red", "blue", "green", "yellow"];
+
+    //Alle vorhandenen Aktionskarten ges. Spiel
+    let actionCards: string[] = ["+2", "<>", "A"]; // 12 stk / 2 pro Farbe / A = Aussetzen
+    let jokerCards: string[] = ["FW", "+4"]; // 4 pro Joker FW = Farbe wählen
+    let numberCards: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
     // Alle noch verfügbaren Karten im Spiel
-    var kartendeck: Karte[];
+    var deck: Card[];
     // Karten auf meine Hand
-    var meineKarten: Karte[];
+    var myCards: Card[];
 
-    // Eine Hilfsklasse um eine Karte zu beschreiben (farbe + art)
-    class Karte {
-        farbe: string;
-        art: string;
+    // Eine Klasse um eine Karte zu beschreiben (farbe + art)
+    class Card {
+        color: string;
+        value: string;
 
         constructor( _farbe: string, _art: string ) {
-            this.farbe = _farbe;
-            this.art = _art;
+            this.color = _farbe; //this=zugriff auf "farbe, art"
+            this.value = _art; //this=Eigenreferenz 
         }
     }
 
     // Spiel initialisieren
     function start(): void {
-        var str: string = prompt( "Bitte die Spielkartenanzahl auswählen: ", "5" );
-        var anzahl: number = Number( str );
+        //promt=Eingabefenster
+        var input: string = prompt( "Bitte die Spielkartenanzahl auswählen: ", "5" );
+        var anzahl: number = Number( input );
 
-        erzeugeAlleKarten();
-        meineKarten = [];
+        createAllCards(); //Funktionsaufruf
+        myCards = [];
 
-        for ( var i: number = 0; i < anzahl; i++ ) {
-            zufallskarteZiehen();
+        for ( var i: number = 0; i < anzahl; i++ ) { //5x=5 6=6 100=100
+            pickRandomCard(); //Funktionsaufruf
         }
 
-        meineKartenZeichnen();
+        drawMyCards(); //Funktionsaufruf
     }
 
-    function erzeugeAlleKarten(): void {
-        kartendeck = [];
-        for ( var i: number = 0; i < farben.length; i++ ) {
-            var farbe: string = farben[i];
+    function createAllCards(): void { //kein Rückgabewert
+        deck = [];
+        for ( var i: number = 0; i < cardColors.length; i++ ) { //äußere for-schleife läuft 4x durch
+            var color: string = cardColors[i];
 
             // Aktionskarten
-            for ( var j: number = 0; j < 2; j++ ) {
-                for ( var k: number = 0; k < aktionsKarten.length; k++ ) {
-                    var art: string = aktionsKarten[k];
-                    var karte: Karte = new Karte( farbe, art );
-                    kartendeck.push( karte );
+            for ( var j: number = 0; j < 2; j++ ) { //2 von jeder Farbe
+                for ( var k: number = 0; k < actionCards.length; k++ ) { //3
+                    var art: string = actionCards[k];
+                    var karte: Card = new Card( color, art ); //constructor 
+                    deck.push( karte ); //karte wird auf den stapel gelegt 
                 }
             }
 
             // Zahlenkarten
-            for ( j = 0; j < 4; j++ ) {
-                for ( k = 0; k < zahlenKarten.length; k++ ) {
-                    art = zahlenKarten[k];
-                    karte = new Karte( farbe, art );
-                    kartendeck.push( karte );
+            for ( j = 0; j < 4; j++ ) { //4 von jeder Farbe
+                for ( k = 0; k < numberCards.length; k++ ) {
+                    art = numberCards[k];
+                    karte = new Card( color, art );
+                    deck.push( karte );
                 }
             }
         }
 
         // Jokerkarten
         for ( i = 0; i < 4; i++ ) {
-            for ( j = 0; j < jokerKarten.length; j++ ) {
-                art = jokerKarten[j];
-                karte = new Karte( farbe, art );
-                kartendeck.push( karte );
+            for ( j = 0; j < jokerCards.length; j++ ) {
+                art = jokerCards[j];
+                karte = new Card( null, art );
+                deck.push( karte );
             }
         }
     }
 
-    function zufallskarteZiehen(): void {
-        console.log( "Hello" );
+    function pickRandomCard(): void { //Deklaration
 
-        // Anzahl verbleibender Karten im Kartendeck
-        var anzahlVerbleibenderKartenImStapel: number = kartendeck.length;
+        // Anzahl verbleibender Karten im deck
+        let remainingCardsInDeck: number = deck.length;
 
-        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
-        var index: number = Math.floor( Math.random() * anzahlVerbleibenderKartenImStapel );
+        // Zufälligen Index erzeugen, welche Karte wir aus dem deck nehmen
+        var index: number = Math.floor( Math.random() * remainingCardsInDeck ); //floor schneidet komma zahl weg//
 
-        // Karte aus dem Kartendeck holen
-        var karte: Karte = kartendeck[index];
+        // Karte aus dem deck holen
+        var card: Card = deck[index];
 
         // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
-        kartendeck.splice( index, 1 );
+        deck.splice( index, 1 );
 
-        meineKarten.push( karte );
+        myCards.push( card ); //karte wird auf "meinen Stapel gelegt
     }
 
-    function meineKartenZeichnen(): void {
+    function drawMyCards(): void { //kein Rückgabewert
 
-        for ( var i: number = 0; i < meineKarten.length; i++ ) {
-            var karte: Karte = meineKarten[i];
+        for ( var i: number = 0; i < myCards.length; i++ ) {  //so viele Karten wie auf der Hand, durchlauf
+            var tmpCard: Card = myCards[i];
 
-            //<div class="card"> 
-            let card: HTMLDivElement = document.createElement( "div" );
+            //<div class="card">  //erzeugen vonHTMLtags, karten anzeigen
+            let card: HTMLDivElement = document.createElement( "div" ); //document, html element erstellen typ div
             card.className = "card";
 
             // <div class="bg green"></div> 
             let bg: HTMLDivElement = document.createElement( "div" );
-            bg.className = "bg " + karte.farbe;
-            card.appendChild( bg );
+            bg.className = "bg " + tmpCard.color;
+            card.appendChild( bg ); //fügt den hintergrund dieser Karte hinzu - verschachtelt angefügt
 
-            // <div class="circle"></div> 
+            // <div class="circle"></div> //Ellipse der Karte hinzufügen
             let kreis: HTMLDivElement = document.createElement( "div" );
             kreis.className = "circle";
-            card.appendChild( kreis );
+            card.appendChild( kreis ); //fügt die Ellipse dieser einen Karte hinzu 
 
             //  <div class="top-left">9</div> 
             let obenLinks: HTMLDivElement = document.createElement( "div" );
             obenLinks.className = "top-left";
-            obenLinks.innerHTML = karte.art;
-            card.appendChild( obenLinks );
+            obenLinks.innerHTML = tmpCard.value; //was kommt auf diese karte drauf idF Zahl
+            card.appendChild( obenLinks ); //fügt die zahl oben links dieser Karte hinzu
 
             //  <div class="center">9</div> 
             let zentrum: HTMLDivElement = document.createElement( "div" );
             zentrum.className = "center";
-            zentrum.innerHTML = karte.art;
-            card.appendChild( zentrum );
+            zentrum.innerHTML = tmpCard.value; //was kommt auf diese Karte idF die Zahl zentriert
+            card.appendChild( zentrum ); //fügt die Zahl zentriert dieser Karte hinzu
 
             // <div class="bottom-right">9</div> 
             let untenRechts: HTMLDivElement = document.createElement( "div" );
             untenRechts.className = "bottom-right";
-            untenRechts.innerHTML = karte.art;
+            untenRechts.innerHTML = tmpCard.value;
             card.appendChild( untenRechts );
 
             let style: CSSStyleDeclaration = card.style;
             style.left = 10 + i * 80 + "px";
             style.bottom = "10px";
 
-            document.body.appendChild( card );
+            document.body.appendChild( card ); //Karte HTML body hinzufügen
         }
     }
 
-    document.addEventListener( "DOMContentLoaded", start );
+    document.addEventListener( "DOMContentLoaded", start ); //
 }
